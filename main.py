c96a374322a408c3615364a581ef31a38fcbe59a
hawariyaw paulos <hawipaul308@gmail.com>
2024-02-01 13:54:31 -0800

    madlib

---
+++ main.py
@@ -0,0 +1,17 @@
+# creating a madlib
+# using format for string concatination
+"""
+print("subscribe to {}".format("me"))
+adj = input("input an adjective: ")
+hate = input("put the programming languge you hate: ")
+madlib = (f"i love python programming so {adj}, and also i love java so {adj} but, "
+          f"really  {hate} programing language!")
+print(madlib)
+
+road = input("road name: ")
+house = input("house name: ")
+dog = input("dog name: ")
+mindmap = (f"go from the house straight to the {road} and then you will find {house} after that go "
+           f"chase the {dog} ")
+print(mindmap)
+"""
